cb0184c90c35e82ee11f49650a6897a5729a1d66
Huxley_Millard <gerogiamillardccm@gmail.com>
2019-02-04 18:14:59 +0000

    Post can store and index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'photo/index'
+  post 'photo/store'
   devise_for :users, path: '', path_names: { sign_in: "login", sign_out: "logout", sign_up: "signup"}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+   
+
+
 
   root 'home#index'
 end
