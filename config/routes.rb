085a9ddfa8ae30ef68d811806f05cf59f5723189
Huxley_Millard <gerogiamillardccm@gmail.com>
2019-02-07 17:19:00 +0000

    migrated active record, checked config

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'photo/index'
+  root 'photo#index'
   post 'photo/store'
   devise_for :users, path: '', path_names: { sign_in: "login", sign_out: "logout", sign_up: "signup"}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
 
 
 
-  root 'home#index'
+  # root 'home#index'
 end
